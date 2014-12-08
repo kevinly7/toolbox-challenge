7f559d21f4c6b9563e0d668614a6d0c1dec8474e
Kevin Ly <kevinly7@uw.edu>
2014-12-07 23:04:22 -0800

    finally finished thisgit add -Agit add -A

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 "use strict"
 
+
 $(document).ready(function() {
-
-
+    var timer;
     createBoard();
-
+    $('#how-to').click(function() {
+        displayHowMessage();
+    });
     $('#start').click(function() {
+        window.clearInterval(timer);
+        resetGame()
         var matches = 0;
         var missed = 0;
         var remaining = 8;
         var clickedTiles = [];
+        var clickedImg = [];
         var clickable = true;
-
+        var startTime = _.now();
+        timer = window.setInterval(function() {
+            var elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
+            $('#elapsed-seconds').text("Time Elapsed: " + elapsedSeconds);
+        }, 1000);
         $('#game-board img').click(function() {
-            var startTime = _.now();
-            var timer = window.setInterval(function() {
-                var elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
-                $('#elapsed-seconds').text(elapsedSeconds);
-
-                if (win) {
-                    window.clearInterval(timer);
-                }
-            }, 1000);
             if(clickable) {
                 var img = $(this);
                 var tile = img.data('tile');
@@ -29,35 +29,40 @@ $(document).ready(function() {
                 if(img.attr('src') == 'img/tile-back.png') {
                     if (clickedTiles.length < 2) {
                         clickedTiles.push(tile);
+                        clickedImg.push(img);
                     }
                     flipImage(img, tile);
                     if (clickedTiles.length == 2) {
                         clickable = !clickable;
-                        if (clickedTiles[0].tileNum == clickedTiles[1].tileNum) {
-                            matches++;
-                            remaining--;
-                            $('#matches').text('Matches: ' + matches);
-                            $('#remaining').text('Remaining ' + remaining);
-                            if (matches == 8) {
-                                window.clearInterval(timer);
-                                //$('#winner').show();
+                        window.setTimeout(function() {
+                            if (clickedTiles[0].tileNum == clickedTiles[1].tileNum) {
+                                matches++;
+                                remaining--;
+                                $('#matches').text('Matches: ' + matches);
+                                $('#remaining').text('Remaining: ' + remaining);
+                                if (matches == 8) {
+                                    window.clearInterval(timer);
+                                    displayWinMessage();
+                                }
+                            } else {
+                                missed++;
+                                $('#misses').text('Missed: ' + missed);
+                                flipImage(clickedImg[0], clickedTiles[0]);
+                                flipImage(clickedImg[1], clickedTiles[1]);
                             }
-                        } else {
-                            missed++;
-                            $('#misses').text('Missed: ' + missed);
-                            flipImage(clickedTiles[0]);
-                            flipImage(clickedTiles[1]);
-                        }
-                        clickedTiles = [];
-                        //clickedIMGs = [];
-                        clickable = !clickable;
-                    }
-                }
-            }
+                            clickedTiles = [];
+                            clickedImg = [];
+                            clickable = !clickable;
+                        }, 1000);
+                    } //if two tiles are clicked
+                } //if clicked on an unflipped tile
+            } //if image is clickable
         }); //on click of gameboard images
-    });
+    }); //on click of start button
 }); //jQuery Ready Function
 
+
+// shuffles the images and builds the gameboard
 function createBoard() {
     var tiles = [];
     var idx;
@@ -104,6 +109,7 @@ function createBoard() {
     gameBoard.append(row);
 }
 
+// flips the images
 function flipImage(img, tile) {
     img.fadeOut(100, function () {
         if (tile.flipped) {
@@ -115,8 +121,34 @@ function flipImage(img, tile) {
         img.fadeIn(100);
         tile.flipped = !tile.flipped;
     });
-    //img.fadeOut(100, function () {
-    //    img.attr('src', source);
-    //    img.fadeIn(100);
-    //}); //after fadeOut
+}
+
+// displays the win message
+function displayWinMessage() {
+    $('#win').css("display", "block");
+    $('main').css("opacity", "0.4");
+    $('#win').click(function() {
+        resetGame();
+    });
+}
+
+// resets the game to play again
+function resetGame() {
+    $('main').css("opacity", "1");
+    $('#win').css("display", "none");
+    $('#game-board').empty();
+    createBoard();
+    $('#elapsed-seconds').text("Time Elapsed: " + 0);
+    $('#matches').text('Matches: ' + 0);
+    $('#remaining').text('Remaining: ' + 8);
+    $('#misses').text('Missed: ' + 0);
+}
+
+function displayHowMessage() {
+    $('#instructions').css("display", "block");
+    $('main').css("opacity", "0.4");
+    $('#close-icon').click(function () {
+        $('main').css("opacity", "1");
+        $('#instructions').css("display", "none");
+    });
 }
